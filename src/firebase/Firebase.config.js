5ddbb13eea6d17da7b27e8b7fca0ca8f7f670642
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-07-16 04:42:30 +0600

    hide

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -7,20 +7,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDKs9pSwJzu4daPp-Vb4P9yijRxlpsAW9k",
-  authDomain: "restaurant-8b3b0.firebaseapp.com",
-  projectId: "restaurant-8b3b0",
-  storageBucket: "restaurant-8b3b0.appspot.com",
-  messagingSenderId: "927788074486",
-  appId: "1:927788074486:web:46a0aa07687bb73f5af3d7",
 
 
-//   apiKey:import.meta.env.VITE_apiKey,
-//   authDomain:import.meta.env.VITE_authDomain,
-//   projectId:import.meta.env.VITE_projectId,
-//   storageBucket:import.meta.env.VITE_storageBucket,
-//   messagingSenderId:import.meta.env.VITE_messagingSenderId,
-//   appId:import.meta.env.VITE_appId,
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
